881f653ca49a69d836739a8bc24f5978340102a8
Joe Kim <joesm.genie@gmail.com>
2023-09-20 17:41:43 +0700

    Add create update dto for module teacher

--- src/modules/teachers/dto/create-teacher.dto.ts
+++ src/modules/teachers/dto/create-teacher.dto.ts
@@ -1,1 +1,47 @@
-export class CreateTeacherDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Gender } from '@prisma/client';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateTeacherDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  @ApiProperty({ required: true })
+  first_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  @ApiProperty({ required: true })
+  last_name: string;
+
+  @IsEnum(Gender)
+  @IsNotEmpty()
+  @ApiProperty({ required: true, enum: Gender })
+  gender: Gender;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(100)
+  @ApiProperty({ required: true })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(10)
+  @ApiProperty({ required: true })
+  phone_number: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(255)
+  bio: string;
+}
--- src/modules/teachers/dto/update-teacher.dto.ts
+++ src/modules/teachers/dto/update-teacher.dto.ts
@@ -1,4 +1,29 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTeacherDto } from './create-teacher.dto';
+import { IsString, IsOptional, MaxLength, IsEmail } from 'class-validator';
 
-export class UpdateTeacherDto extends PartialType(CreateTeacherDto) {}
+export class UpdateTeacherDto {
+  @IsString()
+  @IsOptional()
+  @MaxLength(100)
+  first_name: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(100)
+  last_name: string;
+
+  @IsString()
+  @IsOptional()
+  @IsEmail()
+  @MaxLength(100)
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(10)
+  phone_number: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(255)
+  bio: string;
+}
