b18ab9f6fa440a880b475c82ad12f83bd0839aeb
Joe Kim <joesm.genie@gmail.com>
2023-09-20 15:37:25 +0700

    Initial students module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabasesModule } from './databases/databases.module';
+import { StudentsModule } from './modules/students/students.module';
 
 @Module({
-  imports: [DatabasesModule],
+  imports: [DatabasesModule, StudentsModule],
   controllers: [],
   providers: [],
 })
