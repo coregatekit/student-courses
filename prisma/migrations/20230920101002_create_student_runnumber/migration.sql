1cbd1d3a4475ef065357e42ad3d5853b4f8f5a4a
Joe Kim <joesm.genie@gmail.com>
2023-09-20 17:10:34 +0700

    Add schema student runnumber

---
+++ prisma/migrations/20230920101002_create_student_runnumber/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "student_runnumber" (
+    "id" INTEGER NOT NULL,
+    "current_number" INTEGER NOT NULL,
+
+    CONSTRAINT "student_runnumber_pkey" PRIMARY KEY ("id")
+);
