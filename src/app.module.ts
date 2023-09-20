54be09f854bd7043c8a88c739ecf906198e0ee4a
Joe Kim <joesm.genie@gmail.com>
2023-09-20 17:29:33 +0700

    Setup config service for root

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { DatabasesModule } from './databases/databases.module';
 import { StudentsModule } from './modules/students/students.module';
 import { HealthModule } from './modules/health/health.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabasesModule, HealthModule, StudentsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabasesModule,
+    HealthModule,
+    StudentsModule,
+  ],
   controllers: [],
   providers: [],
 })
