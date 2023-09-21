915a238e2d569f3ab090eff12bc9b5c08ebd58a1
Joe Kim <joesm.genie@gmail.com>
2023-09-21 13:41:32 +0700

    Import database module to courses module

--- src/modules/courses/courses.module.ts
+++ src/modules/courses/courses.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
+import { DatabasesModule } from 'src/databases/databases.module';
 
 @Module({
+  imports: [DatabasesModule],
   controllers: [CoursesController],
-  providers: [CoursesService]
+  providers: [CoursesService],
 })
 export class CoursesModule {}
