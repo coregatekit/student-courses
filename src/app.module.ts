d22780deb145b74974a45b1d54e37208317af4fa
Joe Kim <joesm.genie@gmail.com>
2023-09-21 11:51:02 +0700

    Initial courses module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { StudentsModule } from './modules/students/students.module';
 import { HealthModule } from './modules/health/health.module';
 import { ConfigModule } from '@nestjs/config';
 import { TeachersModule } from './modules/teachers/teachers.module';
+import { CoursesModule } from './modules/courses/courses.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { TeachersModule } from './modules/teachers/teachers.module';
     HealthModule,
     StudentsModule,
     TeachersModule,
+    CoursesModule,
   ],
   controllers: [],
   providers: [],
