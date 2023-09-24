daf39bdbc6f83bcf6474e78e845a01eba1224f1e
Joe Kim <joesm.genie@gmail.com>
2023-09-24 16:25:42 +0700

    Add dto create register course

--- src/modules/register-course/dto/create-register-course.dto.ts
+++ src/modules/register-course/dto/create-register-course.dto.ts
@@ -1,1 +1,11 @@
-export class CreateRegisterCourseDto {}
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateRegisterCourseDto {
+  @IsNumber()
+  @IsNotEmpty()
+  student_id: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  course_id: number;
+}
