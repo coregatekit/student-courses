7959144bdd059638e1378ec680b1f8cff7559680
Joe Kim <joesm.genie@gmail.com>
2023-09-24 16:45:45 +0700

    Add flag api property for swagger

--- src/modules/courses/dto/create-course.dto.ts
+++ src/modules/courses/dto/create-course.dto.ts
@@ -1,20 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
 
 export class CreateCourseDto {
   @IsNotEmpty()
   @IsString()
   @MaxLength(150)
+  @ApiProperty({ required: true })
   name: string;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({ required: true })
   level: number;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({ required: true })
   teacher_id: number;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({ required: true })
   category_id: number;
 }
--- src/modules/register-course/dto/create-register-course.dto.ts
+++ src/modules/register-course/dto/create-register-course.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateRegisterCourseDto {
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({ required: true })
   student_id: number;
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({ required: true })
   course_id: number;
 }
