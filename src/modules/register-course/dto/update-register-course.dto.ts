64e8b2cb5237266082f4a587cb3fd0074dee87e6
Joe Kim <joesm.genie@gmail.com>
2023-09-24 16:25:52 +0700

    Remove unuse dto

--- src/modules/register-course/dto/update-register-course.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateRegisterCourseDto } from './create-register-course.dto';
-
-export class UpdateRegisterCourseDto extends PartialType(CreateRegisterCourseDto) {}
